824e0fdc0e0056c3ddd0c97c5c7e46a84046fed3
keyurparalkar <keur.plkar@gmail.com>
2020-11-19 22:23:05 +0530

    Added feature to dynamically count children

--- src/Components/HScrollGrid.js
+++ src/Components/HScrollGrid.js
@@ -4,17 +4,25 @@ import '../Stylesheets/HScrollGrid.css';
 class HScrollGrid extends React.Component{
     constructor(props){
         super(props);
+
+        this.hscrollRef = React.createRef();
     }
 
+    componentDidMount(){
+        let n = this.hscrollRef.current.children.length;
+        this.hscrollRef.current.style.setProperty('--total', n);
+        
+        console.log(`CHILDRENS  = ${n}`);
+    }
     render(){
         return(
-            <div className="hscroll-grid">
-                <div className="card">test</div>
-                <div className="card">test</div>
-                <div className="card">test</div>
-                <div className="card">test</div>
-                <div className="card">test</div>
-            </div>
+            <ul className="hscroll-grid" ref={this.hscrollRef}>
+                <li className="card">test</li>
+                <li className="card">test</li>
+                <li className="card">test</li>
+                <li className="card">test</li>
+                <li className="card">test</li>
+            </ul>
         );
     }
 }
